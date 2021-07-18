da3b3a80d666f2f97ca928ed9a3fa52157ae6f3f
Meisam <meisamasady2002@gmail.com>
2021-07-18 07:44:39 -0700

    add Number class

---
+++ src/main/java/model/Number.java
@@ -0,0 +1,19 @@
+package model;
+
+public class Number {
+    private double realPart;
+    private double imaginaryPart;
+
+    public Number(double realPart, double imaginaryPart) {
+        this.realPart = realPart;
+        this.imaginaryPart = imaginaryPart;
+    }
+
+    public double getRealPart() {
+        return realPart;
+    }
+
+    public double getImaginaryPart() {
+        return imaginaryPart;
+    }
+}
