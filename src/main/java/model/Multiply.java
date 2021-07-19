236843191620bee748dd035d6eb6fcbe9b5a4805
Meisam <meisamasady2002@gmail.com>
2021-07-19 08:27:58 -0700

    complete Multiply class

--- src/main/java/model/Multiply.java
+++ src/main/java/model/Multiply.java
@@ -5,16 +5,14 @@ public class Multiply {
 
     public Number multiply(Number firstNumber,Number secondNumber)
     {
+//(a+b*i)(x+y*i)=ax-by+(ay+bx)*i
+
+     double realPart=(firstNumber.getRealPart()*secondNumber.getRealPart())-(firstNumber.getImaginaryPart() *secondNumber.getImaginaryPart());
+     double imaginaryPart=(firstNumber.getRealPart()*secondNumber.getImaginaryPart())+(firstNumber.getImaginaryPart()*secondNumber.getRealPart());
 
 
 
-
-
-
-
-
-
-       return null;
+       return new Number(realPart,imaginaryPart);
     }
 
 
